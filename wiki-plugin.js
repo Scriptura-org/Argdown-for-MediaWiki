f690e7b7b708a4212debcebea8959427d40a3d61
James Harris <james.harris@cdbr.org>
2021-02-24 19:41:14 -0600

    Remove linebreaks so SVG displays correctly StdOutPlugin is used to output Argdown to stdout without the use of console.log. We prefer manual output.

--- wiki-plugin.js
+++ wiki-plugin.js
@@ -1,26 +1,21 @@
-import { argdown, StdOutPlugin } from "@argdown/node";
+import { argdown } from "@argdown/node";
 
-argdown.addPlugin(new StdOutPlugin());
 argdown.defaultProcesses["wiki-plugin"] = [
     "parse-input", "build-model", "build-map", "highlight-source", "export-dot", "export-svg", "export-web-component"
 ];
 
-let argument = process.argv[2];
-if (!argument) argument = "";
-
 const request = {
-    input: argument,
+    input: process.argv[2],
     process: "wiki-plugin",
-    html: {
-        headless: true
-    },
-    // webComponent: {
-    //     webComponentScriptUrl: "extensions/Argdown/test/argdown-map.js",
-    //     globalStyleUrl: "extensions/Argdown/test/argdown-map.css",
-    //     webComponentPolyfillUrl: "extensions/Argdown/test/webcomponents-bundle.js"
-    // }
 };
 
 const response = argdown.run(request);
+var webComponent = response.webComponent;
 
-console.log(response.webComponent); // output to MediaWiki wikitext via stdout
+// Clean the webComponent
+webComponent = webComponent.replace(/<!--[\s\S]*?-->/g, "") // remove comments
+var pieces = webComponent.split("<svg");
+pieces[1] = pieces[1].replace(/\r?\n|\r/g,""); // remove linebreaks from <svg> tag
+webComponent = pieces.join("<svg");
+
+console.log(webComponent); // output to MediaWiki wikitext via stdout
